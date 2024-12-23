9dbf7a527a74ac97b9ddb6a4d82d23721f49f0d8
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-12-23 16:34:57 -0300

    Types testing...

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
 let age = 16;
-console.log(`${age}: ${typeof age}`);
+console.log(`Age variable:${age}: ${typeof age}`);
+let newAge = age;
+while (newAge < 50) {
+    newAge += 5;
+    console.log(`\n${newAge}`);
+}
+age = newAge;
+console.log(`\nAge variable redefined:\n${age}: ${typeof age}`);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 let age: number = 16;
 
-console.log(`${age}: ${typeof age}`);
+console.log(`Age variable:${age}: ${typeof age}`);
+
+let newAge: number = age;
+
+while (newAge < 50) {
+    newAge += 5;
+    console.log(`\n${newAge}`);
+}
+
+age = newAge;
+console.log(`\nAge variable redefined:\n${age}: ${typeof age}`);
