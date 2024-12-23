14483a650af434e0f87791d8499c81f04b1f3011
Adriano Lima Rossi <adriano.lmrs1@gmail.com>
2024-12-23 17:19:23 -0300

    Update array.ts

--- src/array.ts
+++ src/array.ts
@@ -1,12 +1,41 @@
-const main = (...args: any[]) => {
-    let numbers: number[] = [1, 2, 3, 4, 5];
-    console.log(`${numbers}: ${typeof numbers}`);
-    
-    let user: [number, string, string] = [args[0], args[1], args[2]]
-    user.forEach((index) => {console.log(`\n${index}: ${typeof index}`);})
-    
-    user.push(69)
-    user.forEach((index) => {console.log(`\n${index}: ${typeof index}`);})
+interface User {
+    bYear: number;
+    name: string;
+    email: string;
+  }
+  
+  const defaultUser: User = {
+    bYear: 2008,
+    name: 'Adriano',
+    email: 'adriano@example.com'
+  };  
+
+const main = (user: User, ...args: any[]) => {
+  const userArray = [user.bYear, user.name, user.email];
+  const other = [...args]; // other arguments
+
+  // log user values
+  console.log("User Array:");
+  userArray.forEach((value, index) => {
+    console.log(`${index}: ${value} (${typeof value})`);
+  });
+
+  // add array extra value
+  let extraNum: number = Math.floor(Math.random() * 10);
+  enum extraArray { number1 = extraNum, number2 = extraNum ** 2, number3 = extraNum ** extraNum };
+  userArray.push(extraArray.number1, extraArray.number2, extraArray.number3);
+  console.log("\nUser Array After Push:");
+  userArray.forEach((value, index) => {
+    console.log(`${index}: ${value} (${typeof value})`);
+  });
+
+  // ...args
+  if (other.length > 0) {
+    console.log("\nAdditional Arguments:");
+    other.forEach((value, index) => {
+      console.log(`${index}: ${value} (${typeof value})`);
+    });
+  }
 }
 
-main(2008, 'Adriano', 'adriano@example.com');
+main(defaultUser, 'asdasdasd', 6969);
